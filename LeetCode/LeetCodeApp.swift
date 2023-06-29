5ef6feab702d113cca3005c1861526010470d7f9
Sergey Koriukin <s.koriukin@webinar.ru>
2023-06-29 09:46:17 +0300

    Initial Commit

---
+++ LeetCode/LeetCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeApp.swift
+//  LeetCode
+//
+//  Created by Sergey Koriukin on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
